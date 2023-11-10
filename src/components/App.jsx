c7b66b236f3d30a84766264c8e63cb1215254ae6
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-11-10 18:53:59 +0200

    add navigate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 
 const Home = lazy(() => import('../pages/Home/Home'));
@@ -19,6 +19,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
